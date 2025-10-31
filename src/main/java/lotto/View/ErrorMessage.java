366fe64ec74ffb1e7e35a36f339105d32efb9a5e
cyscys06 <cyscys06@gmail.com>
2025-11-01 00:14:26 +0900

    feat: 에러 메세지 및 게터 추가

---
+++ src/main/java/lotto/View/ErrorMessage.java
@@ -0,0 +1,20 @@
+package lotto.View;
+
+public enum ErrorMessage {
+    ERROR("[ERROR]"),
+    EMPTYINPUT("빈 문자열을 입력할 수 없습니다."),
+    NOTNUMBER("입력값에 숫자나 구분자 이외의 문자를 입력할 수 없습니다."),
+    NEGATIVE("양수만 입력할 수 있습니다."),
+    NOTUNIT("구매 금액은 1천원 단위로 입력해야 합니다."),
+    NOTUNIQUE("중복 번호를 입력할 수 없습니다.");
+
+    private final String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
