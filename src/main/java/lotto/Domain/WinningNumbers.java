443f86f31c5125e50f6421d9e2ef6f4971fb1cb8
cyscys06 <cyscys06@gmail.com>
2025-11-01 05:37:24 +0900

    feat: 당첨 번호 및 보너스 번호 중 중복이 있는지 검증하는 기능 추가

--- src/main/java/lotto/Domain/WinningNumbers.java
+++ src/main/java/lotto/Domain/WinningNumbers.java
@@ -1,5 +1,8 @@
 package lotto.Domain;
 
+import lotto.View.ErrorMessage;
+
+import java.util.HashSet;
 import java.util.List;
 
 public class WinningNumbers {
@@ -7,8 +10,16 @@ public class WinningNumbers {
     private final int bonusNumber;
 
     WinningNumbers(List<Integer> numbers, int number) {
+        validate_Unique(numbers, number);
         winningNumbers = new Lotto(numbers);
         bonusNumber = number;
     }
 
+    private void validate_Unique(List<Integer> numbers, int number) {
+        HashSet<Integer> validateUnique = new HashSet<>(numbers);
+        validateUnique.add(number);
+        if (validateUnique.size() != LottoInfo.LOTTONUMBER_COUNT.getInfo() + 1) {
+            throw new IllegalArgumentException(ErrorMessage.NOTUNIQUE.getErrorMessage());
+        }
+    }
 }
