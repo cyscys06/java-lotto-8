5663a1155a48869d8ea9b67e37b11c85bf256707
cyscys06 <cyscys06@gmail.com>
2025-11-01 00:07:44 +0900

    feat: 출력 메세지 및 게터 추가

---
+++ src/main/java/lotto/View/Message.java
@@ -0,0 +1,29 @@
+package lotto.View;
+
+public enum Message {
+    REQUEST_PURCHASELOTTO("구입금액을 입력해 주세요."),
+    REQUEST_WINNINGNUMBERS("당첨 번호를 입력해 주세요."),
+    REQUEST_BONUSNUMBER("보너스 번호를 입력해 주세요."),
+    NOTICE_PURCHASELOTTO("개를 구매했습니다."),
+    STATISTICS("당첨 통계"),
+    HR("---"),
+    FIRST_PLACE("6개 일치 (2,000,000,000원)"),
+    SECOND_PLACE("5개 일치, 보너스 볼 일치 (30,000,000원)"),
+    THIRD_PLACE("5개 일치 (1,500,000원)"),
+    FOURTH_PLACE("4개 일치 (50,000원)"),
+    FIFTH_PLACE("3개 일치 (5,000원)"),
+    YIELD("총 수익률은 "),
+    PERCENT("%입니다."),
+    HYPHEN(" - "),
+    SEPARATOR(",");
+
+    private final String message;
+
+    Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
