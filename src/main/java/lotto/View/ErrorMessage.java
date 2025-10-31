7554b90e84433f4e7725bd94ddc05f1a8aee68c1
cyscys06 <cyscys06@gmail.com>
2025-11-01 00:35:27 +0900

    feat: 에러 메세지 추가

--- src/main/java/lotto/View/ErrorMessage.java
+++ src/main/java/lotto/View/ErrorMessage.java
@@ -6,7 +6,9 @@ public enum ErrorMessage {
     NOTNUMBER("입력값에 숫자나 구분자 이외의 문자를 입력할 수 없습니다."),
     NEGATIVE("양수만 입력할 수 있습니다."),
     NOTUNIT("구매 금액은 1천원 단위로 입력해야 합니다."),
-    NOTUNIQUE("중복 번호를 입력할 수 없습니다.");
+    NOTUNIQUE("중복 번호를 입력할 수 없습니다."),
+    NOTINRANGE("1~45 사이의 숫자만 입력해야 합니다."),
+    NOTPROPERCOUNT("로또 번호는 6개여야 합니다.");
 
     private final String errorMessage;
 
