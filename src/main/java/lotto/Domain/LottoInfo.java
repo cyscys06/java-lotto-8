c04f391a84b09fb90f26b6029dc610e04e16c54d
cyscys06 <cyscys06@gmail.com>
2025-11-01 04:31:59 +0900

    feat: 로또 숫자의 정보를 저장하는 열거형 클래스 추가

---
+++ src/main/java/lotto/Domain/LottoInfo.java
@@ -0,0 +1,17 @@
+package lotto.Domain;
+
+public enum LottoInfo {
+    LOTTONUMBER_MIN(1),
+    LOTTONUMBER_MAX(45),
+    LOTTONUMBER_COUNT(6);
+
+    private final int info;
+
+    LottoInfo(int info) {
+        this.info = info;
+    }
+
+    public int getInfo() {
+        return info;
+    }
+}
