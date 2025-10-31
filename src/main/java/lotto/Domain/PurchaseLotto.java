a72f8c52888c19d26fa88faf2d93132bbd0cab9f
cyscys06 <cyscys06@gmail.com>
2025-11-01 01:21:57 +0900

    feat: 구매 금액이 음수인지 검증하는 메서드 추가

---
+++ src/main/java/lotto/Domain/PurchaseLotto.java
@@ -0,0 +1,22 @@
+package lotto.Domain;
+
+import lotto.View.ErrorMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PurchaseLotto {
+    private static final int UNIT = 1000;
+    private final List<Lotto> LottoList;
+    private int purchaseAmount;
+
+    PurchaseLotto() {
+        LottoList = new ArrayList<>();
+    }
+
+    public void validate_Negative(int purchaseMoney) {
+        if (purchaseMoney < 0) {
+            throw new IllegalArgumentException(ErrorMessage.NEGATIVE.getErrorMessage());
+        }
+    }
+}
