82e818efb0fd011e0672901264cba7dc5950a0b9
cyscys06 <cyscys06@gmail.com>
2025-11-01 00:59:39 +0900

    feat: 문자열 배열의 원소를 각각 정수로 변환하여 정수 리스트로 반환하는 기능 추가

--- src/main/java/lotto/Service/ProcessInput.java
+++ src/main/java/lotto/Service/ProcessInput.java
@@ -2,8 +2,19 @@ package lotto.Service;
 
 import lotto.View.Message;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProcessInput {
     public String[] splitString(String input) {
         return input.split(Message.SEPARATOR.getMessage());
+    }
+
+    public List<Integer> StringtoInteger(String[] splitString) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String string : splitString) {
+            numbers.add(Integer.parseInt(string));
+        }
+        return numbers;
     }
 }
