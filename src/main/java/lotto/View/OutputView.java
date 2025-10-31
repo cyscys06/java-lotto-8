e57aadf51dde45db45cdab72634a257d928ea5fc
cyscys06 <cyscys06@gmail.com>
2025-11-01 00:20:49 +0900

    feat: 당첨번호 입력 요구 메서드 추가

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -4,4 +4,9 @@ public class OutputView {
     public void requestPurchaseLotto() {
         System.out.println(Message.REQUEST_PURCHASELOTTO.getMessage());
     }
+
+    public void requestWinningNumbers() {
+        System.out.println(Message.REQUEST_WINNINGNUMBERS.getMessage());
+    }
+
 }
