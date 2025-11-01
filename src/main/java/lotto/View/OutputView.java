91b77481507332b6ae94f098a4e8454a2068df03
cyscys06 <cyscys06@gmail.com>
2025-11-01 22:03:59 +0900

    feat: 로또 별 당첨 여부를 출력하는 기능 추가

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -2,6 +2,10 @@ package lotto.View;
 
 import lotto.Domain.Lotto;
 import lotto.Domain.PurchaseLotto;
+import lotto.Domain.WinningNumbers;
+import lotto.Domain.WinningPrize;
+
+import java.util.HashMap;
 
 public class OutputView {
     public void requestPurchaseLotto() {
@@ -22,5 +26,31 @@ public class OutputView {
         for (Lotto lotto : purchaseLotto.getLottoList()) {
             System.out.println(lotto.getNumbers());
         }
+    }
+
+    public void showCorrectResult(HashMap<WinningNumbers, Integer> correctLottoList) {
+        System.out.println(Message.STATISTICS.getMessage());
+        System.out.println(Message.HR.getMessage());
+
+        System.out.println(Message.FIFTH_PLACE.getMessage()
+                + Message.HYPHEN.getMessage()
+                + correctLottoList.get(WinningPrize.FIFTH)
+                + Message.COUNT.getMessage());
+        System.out.println(Message.FOURTH_PLACE.getMessage()
+                + Message.HYPHEN.getMessage()
+                + correctLottoList.get(WinningPrize.FOURTH)
+                + Message.COUNT.getMessage());
+        System.out.println(Message.THIRD_PLACE.getMessage()
+                + Message.HYPHEN.getMessage()
+                + correctLottoList.get(WinningPrize.THIRD)
+                + Message.COUNT.getMessage());
+        System.out.println(Message.SECOND_PLACE.getMessage()
+                + Message.HYPHEN.getMessage()
+                + correctLottoList.get(WinningPrize.SECOND)
+                + Message.COUNT.getMessage());
+        System.out.println(Message.FIRST_PLACE.getMessage()
+                + Message.HYPHEN.getMessage()
+                + correctLottoList.get(WinningPrize.FIRST)
+                + Message.COUNT.getMessage());
     }
 }
