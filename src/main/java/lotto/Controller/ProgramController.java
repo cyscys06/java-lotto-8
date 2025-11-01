b097827134fef06a64344466ed339749825582cd
cyscys06 <cyscys06@gmail.com>
2025-11-01 23:15:35 +0900

    feat: 당첨 번호를 입력받아 저장하는 기능 추가

--- src/main/java/lotto/Controller/ProgramController.java
+++ src/main/java/lotto/Controller/ProgramController.java
@@ -1,22 +1,30 @@
 package lotto.Controller;
 
+import camp.nextstep.edu.missionutils.Console;
+import lotto.Domain.Lotto;
 import lotto.Domain.PurchaseLotto;
+import lotto.Domain.WinningNumbers;
+import lotto.Service.CompareLottoService;
 import lotto.Service.ProcessInput;
 import lotto.Service.PurchaseLottoService;
 import lotto.View.InputView;
 import lotto.View.OutputView;
+
+import java.util.List;
 
 public class ProgramController {
     private OutputView outputView;
     private InputView inputView;
     private ProcessInput processInput;
     private PurchaseLottoService purchaseLottoService;
+    private CompareLottoService compareLottoService;
 
     public ProgramController() {
         outputView = new OutputView();
         inputView = new InputView();
         processInput = new ProcessInput();
         purchaseLottoService = new PurchaseLottoService();
+        compareLottoService = new CompareLottoService();
     }
 
     private PurchaseLotto PurchaseLotto() {
@@ -31,6 +39,41 @@ public class ProgramController {
                 outputView.showPurchaseLottoList(purchaseLotto);
                 return purchaseLotto;
             } catch (IllegalArgumentException e) {
+                outputView.requestInputAgain(e);
+            }
+        }
+    }
+
+    private List<Integer> saveWinningNumbers() {
+        while (true) {
+            try {
+                outputView.requestWinningNumbers();
+                String winningnumbersInput = inputView.getInput();
+                return processInput.StringtoInteger(
+                        processInput.splitString(winningnumbersInput));
+            } catch (IllegalArgumentException e) {
+                outputView.requestInputAgain(e);
+            }
+        }
+    }
+
+    private int compareLottoList(PurchaseLotto lottoList) {
+        while (true) {
+            try {
+                outputView.requestWinningNumbers();
+                String winningnumbersInput = inputView.getInput();
+                List<Integer> numbers = processInput.StringtoInteger(
+                        processInput.splitString(winningnumbersInput));
+
+                outputView.requestBonusNumber();
+                String bonusNumberInput = inputView.getInput();
+                int bonusNumber = processInput.StringtoInteger(bonusNumberInput);
+
+                WinningNumbers winningNumbers = new WinningNumbers(numbers, bonusNumber);
+
+
+            }
+            catch (IllegalArgumentException e) {
                 outputView.requestInputAgain(e);
             }
         }
