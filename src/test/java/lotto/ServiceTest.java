20191ba022a6c576a90fcfcb29e70a957bcdc50b
cyscys06 <cyscys06@gmail.com>
2025-11-02 04:56:22 +0900

    test: 로또_구매_테스트 추가

--- src/test/java/lotto/ServiceTest.java
+++ src/test/java/lotto/ServiceTest.java
@@ -1,20 +1,31 @@
 package lotto;
 
+import lotto.Domain.Lotto;
+import lotto.Domain.PurchaseLotto;
+import lotto.Domain.WinningNumbers;
 import lotto.Service.ProcessInput;
+
 import static org.assertj.core.api.Assertions.*;
+
+import lotto.Service.PurchaseLottoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class ServiceTest {
     private ProcessInput processInput;
+    private PurchaseLottoService purchaseLottoService;
 
     @BeforeEach
     void setUp() {
         processInput = new ProcessInput();
+        purchaseLottoService = new PurchaseLottoService();
     }
 
     @Nested
@@ -46,4 +57,28 @@ public class ServiceTest {
             assertThat(numbers.get(2)).isEqualTo(3);
         }
     }
+
+    @Nested
+    @DisplayName("로또 구매 테스트")
+    class 로또_구매_테스트 {
+        @Test
+        void 로또_생성_테스트() {
+            assertRandomUniqueNumbersInRangeTest(
+                    () -> {
+                        assertThat(purchaseLottoService.makeLotto())
+                                .containsExactly(1, 5, 8, 23, 33, 45);
+                    }, List.of(8, 23, 45, 1, 5, 33)
+            );
+        }
+
+        @Test
+        void 로또_리스트_생성_테스트() {
+            int purchaseMoney = 14000;
+            PurchaseLotto purchaseLotto = new PurchaseLotto(purchaseMoney);
+            assertThat(purchaseLotto.getLottoList()).hasSize(14);
+        }
+    }
+
+
+
 }
